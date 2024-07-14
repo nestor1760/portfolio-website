39bd192e7d9910dc3e8d147d7156070122a47aa3
Roman Nesterchuk <133716724+nestor1760@users.noreply.github.com>
2024-07-14 16:16:07 +0200

    Update switcherSlices.ts

--- src/store/switcherSlices.ts
+++ src/store/switcherSlices.ts
@@ -2,24 +2,12 @@ import { createSlice } from "@reduxjs/toolkit";
 import { getCookie, setCookie } from "../cookies/utills";
 import { ISwitcherState } from "./interfaces";
 
-// //to save swicther value to localStorage
-// const saveToLocalStorage = (switcher: boolean) => {
-//   localStorage.setItem('switcher', JSON.stringify(switcher));
-// }
-
-// //get switcher value from localStorage
-// const loadFromLocalStorage = () => {
-//   const switcher = localStorage.getItem('switcher');
-//   return switcher ? JSON.parse(switcher) : true;
-// }
-
 const getSwitcherFromCookies = () => {
   const switcherValue = getCookie('theme')
   return switcherValue === 'true' ? true : false
 }
 
 const initialState: ISwitcherState = {
-  // switcher: loadFromLocalStorage(),
   switcher: getSwitcherFromCookies()
 }
 
@@ -29,7 +17,6 @@ const switcherSlice = createSlice({
   reducers: {
     toggleSwitcher: (state) => {
       state.switcher = !state.switcher;
-      // saveToLocalStorage(state.switcher);
       setCookie('theme', state.switcher.toString(), 7);
     }
   },
