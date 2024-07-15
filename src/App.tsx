7a430c52434826ba74041284b43f0faf0c95c474
Nestor1760 <romannesterchuk01gmail.com>
2024-07-15 22:22:51 +0200

    add: react-ga4

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,16 @@ import { Wrapper } from "./styledTags/Wrapper/Wrapper"
 import Layout from "./components/General/Components/Layout/Layout"
 import PrivacyPolicy from "./components/General/Components/PrivacyPolicy/PrivacyPolicy"
 import { useEffect } from "react"
+import ReactGA from 'react-ga4'
 
 function App() {
   const { pathname } = useLocation()
+  const TRACKING_ID = import.meta.env.VITE_PRIVATE_GOOGLE_ID
+
+  useEffect(() => {
+    ReactGA.initialize(TRACKING_ID)
+    ReactGA.send({ hitType: "pageview", page: "/", title: "Home page" })
+  }, [])
 
   useEffect(() => {
     window.scrollTo(0, 0)
