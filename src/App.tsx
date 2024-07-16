62d273ff8153c8a3f51cd64caccf48071b4831e7
Nestor1760 <romannesterchuk01gmail.com>
2024-07-16 23:52:06 +0200

    delete: ga4

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,13 @@ import { Wrapper } from "./styledTags/Wrapper/Wrapper"
 import Layout from "./components/General/Components/Layout/Layout"
 import PrivacyPolicy from "./components/General/Components/PrivacyPolicy/PrivacyPolicy"
 import { useEffect } from "react"
-import ReactGA from 'react-ga4'
 
 function App() {
   const { pathname } = useLocation()
-  const TRACKING_ID = 'G-JDQ37TKSPW'
-
-  useEffect(() => {
-    ReactGA.initialize(TRACKING_ID);
-    ReactGA.send({ hitType: "pageview", page: pathname, title: "Landing Page" });
-  }, [])
 
   useEffect(() => {
     window.scrollTo(0, 0)
   }, [pathname])
-
 
   return (
     <Wrapper>
