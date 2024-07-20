8b163ac54d1b56d3139f27efb741b53dae4cc13e
Nestor1760 <romannesterchuk01gmail.com>
2024-07-20 21:54:47 +0200

    update: cv

--- src/components/General/Components/HomePart/HomeIntro/utills.tsx
+++ src/components/General/Components/HomePart/HomeIntro/utills.tsx
@@ -36,7 +36,7 @@ const IntroUtills = () => {
   const getCV = () => {
     const link = document.createElement('a')
     link.href = '/public/cv/Roman Nesterchuk CV - fronted developer.pdf'
-    link.download = 'roman_nesterchuk_front.pdf'
+    link.download = 'Roman Nesterchuk CV - fronted developer.pdf'
     document.body.appendChild(link)
     link.click()
     document.body.removeChild(link)
